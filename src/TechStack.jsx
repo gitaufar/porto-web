48c2ed88c812b96c2a1f34d992bb1dfec12bb713
gitaufar <zhafiraufar123@gmail.com>
2025-03-02 14:24:35 +0700

    resolve conflict

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -1,11 +1,13 @@
 import CircularGallery from "./blocks/Components/CircularGallery/CircularGallery"
-import BlurText from "./blocks/Components/BlurText"
+import RollingGallery from "./blocks/Components/RollingGallery/RollingGallery"
 import "./TechStack.css"
+
 
 export default function TechStack() {
     return (
         <div className="tech-stack-section" id="tech-stack">
             <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
+            <RollingGallery />
         </div>
     )
 }
