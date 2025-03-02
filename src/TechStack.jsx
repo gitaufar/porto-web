8432148d25d4953714dd2ed9088cc0d10ba0ef64
gitaufar <zhafiraufar123@gmail.com>
2025-03-02 12:44:52 +0700

    upd circular n navbar

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -5,13 +5,6 @@ import "./TechStack.css"
 export default function TechStack() {
     return (
         <div className="tech-stack-section" id="tech-stack">
-            <BlurText 
-            delay={0.2}
-            animateBy="words"
-            direction="top"
-            className="tech-text"
-            text="Tech Stack"
-            />
             <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
         </div>
     )
