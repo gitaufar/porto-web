e5f755eaa2698377d9f764215062adeb04f2272b
gitaufar <zhafiraufar123@gmail.com>
2025-10-16 17:38:13 +0700

    feat(journey): add journey

--- src/Project.jsx
+++ src/Project.jsx
@@ -27,16 +27,11 @@ export default function Project() {
 
     const projects = [
         {
-            title: "My Movie (Personal Project)",
-            description: "A movie website that fetches data from the TMDB API. This project was built to practice working with APIs and managing state related to data fetching using React.js and Tailwind CSS.",
-            githubLink: "https://my-movie-gamma.vercel.app/",
-            type: "website"
-        },
-        {
-            title: "Yess Yogurt (Personal Project)",
-            description: "A website for a yogurt shop built with React.js, TypeScript, and Tailwind CSS. This project was created to deepen my understanding of state management in React using useState adn useContext.",
-            githubLink: "https://yess-ygrt.vercel.app/",
-            type: "website"
+            title: "SwaraIbu (Competition Project)",
+            description:
+                "SwaraIbu is an application designed to assist pregnant mothers in monitoring fetal health through voice recognition technology. The application allows users to record and analyze fetal sounds, providing insights into the baby's condition. SwaraIbu is developed using Kotlin and Jetpack Compose for the user interface, with Firebase as the backend service.",
+            githubLink: "https://github.com/gitaufar/swaraibu-app.git",
+            type: "github"
         },
         {
             title: "Portfolio Website (Personal Project)",
